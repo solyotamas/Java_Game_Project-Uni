6d74faa0c7c597ded97daa56aa04b80d3b3163df
Nagy Laura Blanka <wfa1u7@inf.elte.hu>
2025-04-09 14:22:54 +0200

    moving jeep

--- src/main/java/classes/entities/additions/InfoWindow.java
+++ src/main/java/classes/entities/additions/InfoWindow.java
@@ -1,7 +1,9 @@
 package classes.entities.additions;
 
 import classes.entities.animals.Animal;
+import javafx.geometry.Pos;
 import javafx.scene.control.Button;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 
 public class InfoWindow extends VBox {
@@ -12,15 +14,19 @@ public class InfoWindow extends VBox {
         this.setLayoutX(animal.getLayoutX() - 60);
         this.setLayoutY(animal.getLayoutY() - 60);
 
+        Button closeButton = new Button("X");
+        closeButton.getStyleClass().add("close-button");
+
         Button sellAnimalBtn = new Button("Sell animal");
         sellAnimalBtn.getStyleClass().add("info-button");
         sellAnimalBtn.setOnAction(e -> onSellClick.run());
 
+        HBox row = new HBox();
+        row.setSpacing(5);
+        row.setAlignment(Pos.CENTER);
+        row.getChildren().addAll(sellAnimalBtn, closeButton);
 
-
-        this.getChildren().add(sellAnimalBtn);
-
-
+        this.getChildren().add(row);
 
     }
 }
