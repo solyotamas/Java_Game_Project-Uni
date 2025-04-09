5e16a1763301865d57e144849b50ccc98e0fb93d
a6e6h2 <a6e6h2@inf.elte.hu>
2025-04-09 22:35:23 +0200

    rangerpic shown as tourist

--- src/main/java/classes/entities/human/Ranger.java
+++ src/main/java/classes/entities/human/Ranger.java
@@ -7,8 +7,8 @@ import javafx.scene.layout.VBox;
 
 public class Ranger extends Human {
 
-    private static final int frameWidth = 104;
-    private static final int frameHeight = 106;
+    private static final int frameWidth = 40;//104;
+    private static final int frameHeight = 55;//106;
     private static double speed = 0.8;
     private static final String imgURL = "/images/animated/tourist.png";
 
